7fdf4ba0338d1e665ff000a209821c8869b6a1c3
Omar <byr0nic@users.noreply.github.com>
2025-07-04 23:00:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ if upload:
     net_pnl_value = df[pnl_col].sum()
     net_pnl_str = f"(£{abs(net_pnl_value):,.2f})" if net_pnl_value < 0 else f"£{net_pnl_value:,.2f}"
     net_pnl_color = "#FF4B4B" if net_pnl_value < 0 else "#28A745"
-    col3.metric("Net P&L", net_pnl_str, delta_color=net_pnl_color)
+    col3.metric("Net P&L", net_pnl_str)
 
     st.markdown("---")
 
